b88c54eb448386b68f9bd791b8a5ecf22855bfca
Xetro <golob.matevz@gmail.com>
2023-12-06 16:31:12 +0100

    Add day4 part2

---
+++ day4/part2/part2.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var cards = make([]int, 0)
+var winningsPerCard = make(map[int]int)
+var sum = 0
+
+func traverse(hits int, card int) int {
+	if _, ok := winningsPerCard[card]; ok {
+		return winningsPerCard[card]
+	}
+
+	count := 1
+	for i := 1; i <= hits; i++ {
+		next := card + i
+		if next < len(cards) {
+			if _, ok := winningsPerCard[next]; !ok {
+				count += traverse(cards[next], next)
+			} else {
+				count += winningsPerCard[next]
+			}
+		}
+	}
+	winningsPerCard[card] = count
+	sum += count
+	return count
+}
+
+func main() {
+	file, err := os.Open("../input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+			continue
+		}
+
+		picked := make(map[string]bool, 8)
+		hits := 0
+
+		tokens := strings.FieldsFunc(text, func(r rune) bool {
+			return r == ':' || r == '|'
+		})
+
+		for _, s := range strings.Fields(tokens[2]) {
+			picked[s] = true
+		}
+
+		for _, s := range strings.Fields(tokens[1]) {
+			if _, ok := picked[s]; ok {
+				hits++
+			}
+		}
+		cards = append(cards, hits)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for i, v := range cards {
+		traverse(v, i)
+	}
+	fmt.Println("Sum of Ids: ", sum)
+}
